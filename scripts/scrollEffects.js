6d9805ba8870dd6b122a5ecbf9b16d6d11075f82
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-10-31 19:27:04 +0100

    :zap: End of anim planet 2

--- scripts/scrollEffects.js
+++ scripts/scrollEffects.js
@@ -14,8 +14,8 @@ window.addEventListener("scroll", () => {
 
 // responsive ici, car on utilise la taille de l'écran dans le produit en croix
 window.addEventListener("resize", () => {
-    const { scrollTop, clientHeight, clientWidth } = document.body;
-    translateScalePlanet1(clientWidth, clientHeight, scrollTop);
+    const { scrollTop, clientHeight, clientWidth, scrollHeight } = document.body;
+    translateScalePlanet1(clientWidth, clientHeight, scrollTop, scrollHeight);
 });
 
 /* --- FUNCTIONS --- */
@@ -37,17 +37,17 @@ function translateScaleRocket(scrollValue) {
     rocket.style.transform = `translate3d(0, ${-((scrollValue / 23.5) ** 2)}%, 0) scale(${scale})`;
 }
 
-function translateScalePlanet1(clientWidth, clientHeight, scrollValue, scrollHeight) {
+function translateScalePlanet1(clientWidth, clientHeight, scrollValue, scrollHeight, resize) {
     let distanceY =
         (scrollValue * (clientHeight / 2)) / clientHeight - planet1.clientHeight / 2 + planet1.clientHeight / 2;
     let distanceX =
         (scrollValue * (clientWidth / 2)) / clientHeight - planet1.clientWidth / 2 + planet1.clientWidth / 2;
 
-    let scale = (scrollValue * clientWidth) / clientHeight + clientWidth * 0.5;
-
     let rotate = (scrollValue * 180) / scrollHeight;
 
     planet1.style.transform = `translate3d(${-distanceX}px, ${distanceY}px, 0) rotate(${rotate}deg)`;
+
+    let scale = (scrollValue * clientWidth) / clientHeight + clientWidth * 0.5;
 
     planet1.style.width = `${scale}px`;
     planet1.style.height = `${scale}px`;
@@ -57,9 +57,10 @@ function translateScalePlanet1(clientWidth, clientHeight, scrollValue, scrollHei
 
 function scaleRotateFixedPlanet2(clientHeight, scrollValue) {
     let rotate = ((scrollValue - 2 * clientHeight) * 180) / clientHeight;
+    if (rotate >= 540) rotate = 540;
 
     let scale = scrollValue / (clientHeight + clientHeight / 1.5);
-    if (scale > 1) scale = 1;
+    if (scale >= 1) scale = 1;
 
     planet2.style.transform = `scale(${scale}) rotate(${rotate}deg)`;
     // on inverse la rottaion des enfants pour ne pas avoir de rotation
@@ -68,10 +69,16 @@ function scaleRotateFixedPlanet2(clientHeight, scrollValue) {
     planet2Bloc3.style.transform = `rotate(${-rotate}deg)`;
     planet2Bloc4.style.transform = `rotate(${-rotate}deg)`;
 
-    if (scrollValue >= 2 * clientHeight) {
+    if (scrollValue >= 2 * clientHeight && scrollValue <= 5 * clientHeight) {
         planet2.classList.add("fixed-planet-2");
     } else {
         if (planet2.classList.contains("fixed-planet-2")) planet2.classList.remove("fixed-planet-2");
+        // FIN DU BLOC
+        if (scrollValue >= 5 * clientHeight) {
+            scale = 1 - (scrollValue - 5 * clientHeight) / (clientHeight + clientHeight / 1.5);
+
+            planet2.style.transform += `translate3d(0, ${-clientHeight * 3}px, 0) scale(${scale})`;
+        }
     }
 }
 
